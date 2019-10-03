006d0624080938a8f1a6cf9dce69eaf9726e4a3f
AnnyCaroline <annycarolinegnr@gmail.com>
2019-10-03 19:59:43 -0300

    add Book page

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,6 +1,98 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, AsyncStorage, SafeAreaView, Text, TextInput, TouchableOpacity, StyleSheet, Platform, StatusBar } from 'react-native';
 
-export default function Book() {
-    return <View />
+import api from '../services/api';
+
+export default function Book({ navigation }) {
+    const [date, setDate] = useState('');
+    const id = navigation.getParam('id');
+
+    async function handleSubmit() {
+        const user_id = await AsyncStorage.getItem('user');
+
+        console.log(date);
+
+        await api.post(`/spots/${id}/bookings`, {
+            data: date
+        }, {
+                headers: { user_id }
+            })
+
+        Alert.alert('Solicitação de reserva enviada.');
+
+        navigation.navigate('List');
+    }
+
+    function handleCancel() {
+        navigation.navigate('List');
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.label}>DATA DE INTERESSE *</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Qual data você quer reservar?"
+                placeholderTextColor="#999"
+                autoCapitalize="words"
+                autoCorrect={false}
+                value={date}
+                onChangeText={setDate}
+            />
+
+            <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+                <Text style={styles.buttonText}>Solicitar reserva</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={handleCancel} style={[styles.button, styles.cancelButton]}>
+                <Text style={styles.buttonText}>Cancelar</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 30,
+
+        flex: 1,
+        paddingTop: Platform.OS === 'android' && StatusBar.currentHeight,
+    },
+
+    label: {
+        marginTop: 50,
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2
+    },
+
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
+
+    cancelButton: {
+        backgroundColor: '#ccc',
+        marginTop: 10
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
+});
